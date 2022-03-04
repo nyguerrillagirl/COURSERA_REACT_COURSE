13e4a81743f326d89c0c2864b0a5f5053b153809
Lorraine Figueroa <nyguerrillagirl@brainycode.com>
2022-03-04 11:26:15 -0500

    Week 4 - Fetch From Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,6 +5,7 @@ import {
 import { Link } from 'react-router-dom'
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -106,7 +107,7 @@ function RenderDish({ dish }) {
     if (dish !== null) {
         return (
             <Card>
-                <CardImg src={dish.image} alt={dish.name} />
+                <CardImg src={baseUrl + dish.image} alt={dish.name} />
                 <CardBody>
                     <CardTitle>{dish.name}</CardTitle>
                     <CardText>{dish.description}</CardText>
